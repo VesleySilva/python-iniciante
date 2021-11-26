5617a7cbd135f4e2f989c3d5d42a3da1423adab5
Vesley Cesar da Silva <vesleysilva10@gmail.com>
2021-11-26 00:09:24 -0300

    Lista de Desafios em Python
    Desafios propostos pelo o Gustavo Guanabara no Seu Curso em Vídeo!
    Primeira Lógica foi Desenvolvido Por mim 
    Segunda Lógica Desenvolvida pelo Gustavo Guanabara

---
+++ lista_desafios.py
@@ -0,0 +1,32 @@
+#Author: Vesley Cesar da Silva
+#Data: 24/11/2021
+
+# 0 - Desafio que solicita a pessoa que digite o seu nome depois exiba na tela com uma mensagem de boas vindas
+
+# 01 - Desafio que a pessoa digita o seu dia de nascimento,mês e ano exiba na tela se a data informada está correta
+
+# 03 - Realize a soma dos dois números
+
+# 04 - Desafio inserir dois valores inteiros e mostrar a soma desses valores
+
+# 05 - Faça um programa que insere um número e ele exibe na tela o sucessor e antecessor desse número -ok
+
+# 06 - Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada -ok
+
+# 07 - Desenvolva um programa que lê as duas notas de um aluno e calcule a sua média -ok
+
+# 08 - Escreva um programa que lê um valor em metros e exiba ele convertido em centímetros e milímetros -ok
+
+# 09 - Faça um programa que lê um número inteiro qualquer e mostre sua tabuada -ok
+
+# 10 - Crie um programa que lê quantos dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar -ok
+
+# 11 - Faça um programa que lê a largura e a altura de uma parede em metros e calcule a sua área e quantidade de tinta necessária para pintá-la sabendo que cada litro de tinta pinta uma área de 2m quadrados -ok
+
+# 12 - Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto -ok
+
+# 13 - Faça um algoritmo que leia o salário de um funcionário e mostre o seu novo salário, com 15% de aumento -ok
+
+# 14 - Faça um algoritmo que o usuário digita a temperatura em graus Celsius e ele converte para Fahrenheit -ok
+
+# 15 - Faça um algoritmo que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
