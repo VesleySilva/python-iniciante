4a5b97520401ac5c2185f8a097ff4d04bd671d1b
Vesley Cesar da Silva <vesleysilva10@gmail.com>
2021-11-26 00:02:35 -0300

    Dicas de Linguagem Python
    Scripts em python com marcadores para complementar mais os estudos

---
+++ aula06b.py
@@ -0,0 +1,34 @@
+#Author: Vesley Cesar da Silva
+#Data: 23/11/2021
+
+#Mostrar diferentes resultados das variáveis
+
+#Digite um valor inteiro e exiba em forma de float
+#n1 = float(input('Digite um número: '))
+#print('O valor digitado é: ', n1)
+#print('O formato da variável é: ', type(n1))
+
+#Digite um valor inteiro e exiba em forma de bool
+#n2 = bool(input('Digite um número: '))
+#print('O valor digitado é: ', n2)
+#print('O formato da variável é: ', type(n2))
+
+#Digite um valor inteiro e exiba em forma de str
+#n3 = str(input('Digite um número: '))
+#print('O valor digitado é: ', n3)
+#print('O formato da variável é: ', type(n3))
+
+#Digite um valor float e exiba em forma de int
+#n4 = int(input('Digite um número: '))
+#print('O valor digitado é: ', n4)
+#print('O formato da variável é: ', type(n4))
+
+#Digite algo e ele dirá o formato que foi digitado
+#n5 = input('Digite algo: ')
+#print(n5.isnumeric()) #retorna um booleano dizendo se é numérico
+#print(n5.isalpha()) #retorna um booleano dizendo se é caracter
+#print(n5.isalnum()) #retorna um booleano dizendo se é alfanumérico
+#print(n5.isupper()) #retorna um booleano dizendo se todas as letras são maiúsculas
+#print(n5.islower()) #retorna um booleano dizendo se todas as letras são minúsculas
+#print(n5.isprintable()) #retorna um booleano dizendo se pode ser impresso
+#print(n5.isspace()) #retorna um booleano dizendo se tem espaços
---
+++ aula07a.py
@@ -0,0 +1,79 @@
+#Author: Vesley Cesar da Silva
+#Data: 23/11/2021
+
+#Operadores aritméticos e seus valores e suas sequências de execução
+
+#Adição = +
+#Subtração = -
+#Divisão = /
+#Multiplicação = *
+#Potêcia = **
+#Divisão Inteira = //
+#Resto da Divisão = %
+#Igualdade = ==
+
+#Sequência de execução
+# 1 = ()
+# 2 = **
+# 3 = * , / , // , %
+# 4 = + , -
+
+#--------------------------#
+#funções de cálculos
+#pow(3,4) = 64 - potência
+
+#81**(1/2) = 9.0 - raiz quadrada
+#127**(1/3) = 5.026 - raiz cúbica
+#'Oi'+'Olá' = OiOlá - sinal de + concatena strings
+#'Oi'*5 = OiOiOiOiOi - ele multiplica a quantidade de strings
+
+#nome = input('Qual é o seu nome: ')
+#print('Prazer em te conhecer {:>20}!'.format(nome)) - o nome será alinhado em 20 carecteres á direita
+#print('Prazer em te conhecer {:20}!'.format(nome)) - o nome aparecerá em 20 caracteres em espaço brancos
+#print('Prazer em te conhecer {:<20}!'.format(nome)) - o nome será alinhado em 20 carecteres á esquerda
+#print('Prazer em te conhecer {:^20}!'.format(nome)) - o nome será centralizado em 20 carecteres
+#print('Prazer em te conhecer {:=^20}!'.format(nome)) - o nome será centralizado em 20 carecteres colocando igual em todos os espaços brancos
+
+
+
+#Contas Aritméticas
+#n1 = int(input('Digite um valor: '))
+#n2 = int(input('Digite um segundo valor: '))
+#adicao = n1 + n2
+#subtracao = n1 - n2
+#multiplicao = n1 * n2
+#divisao = n1 / n2
+#divisaoint = n1 // n2
+#potencia = n1 ** 2
+#restdiv = n1 % n2
+#igual = n1 == n2
+#resultado1 = (n1*n2)-(n1+n2)*n2
+#resultado2 = (n1**2)*(n1+n1)
+#resultado3 = n1**3 + n2 // n1 - (n2 % n1)
+#resultado4 = pow(n1, n2)
+#resultado5 = 81 ** (1/2)
+
+#print('O valor da soma dos números: ', adicao)
+#print('O valor da subtração dos números: ', subtracao)
+#print('O valor da multiplicação dos números: ', multiplicao)
+#print('O valor da divisão dos números: ', divisao)
+#print('O valor da divisão inteira dos números: ', divisaoint)
+#print('O valor da potenciação dos números: ', potencia)
+#print('O valor do resto da divisão dos números: ', restdiv)
+#print('O valor da igualdade dos números: ', igual)
+#print('O valor do primeiro resultado dos números: ', resultado1)
+#print('O valor do segundo resultado dos números: ', resultado2)
+#print('O valor do terceiro resultado dos números: ', resultado3)
+#print('O valor da potenciação utilizando pow: ', resultado4)
+#print('O valor da raiz quadrada: ', resultado5)
+
+#n1 = int(input('Digite um número: '))
+#n2 = int(input('Digite o segundo número: '))
+#s = n1 + n2
+#m = n1 * n2
+#sb = n1 - n2
+#d = n1 / n2
+#di = n1 // n2
+#e = n1 ** n2
+#print(' A soma é {} \n A multiplicação é {} \n A subtração é {} \n A divisão é {}'.format(s, m, sb, d))
+#print(' A divisão inteira é {} \n A exponenciação é {} '.format(di, e))
